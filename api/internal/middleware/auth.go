db9c5b52ec52af7f727ab4dab2b0473a25123f48
augustjourney <53155077+augustjourney@users.noreply.github.com>
2024-04-29 12:14:51 +0300

    feat: added auth middleware

---
+++ api/internal/middleware/auth.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"context"
+	"createtodayapi/internal/common"
+	"createtodayapi/internal/logger"
+	"createtodayapi/internal/service"
+	"errors"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"strings"
+)
+
+func Auth(ctx *fiber.Ctx, auth *service.Auth) error {
+	authHeader := ctx.Get("authorization")
+
+	if authHeader == "" {
+		return common.DoApiResponse(ctx, 401, nil, nil)
+	}
+
+	authHeaderData := strings.SplitAfterN(authHeader, "Bearer ", 2)
+
+	if len(authHeaderData) < 2 {
+		logger.Log.Warn(fmt.Sprintf("Could not split Bearer token %s", authHeader))
+		return common.DoApiResponse(ctx, 401, nil, nil)
+	}
+
+	token := authHeaderData[1]
+
+	err, user := auth.ValidateJWTToken(context.Background(), token)
+
+	if err != nil {
+		if errors.As(err, &jwt.ErrTokenExpired) {
+			logger.Log.Warn("Token expired")
+			return common.DoApiResponse(ctx, 403, nil, nil)
+		}
+		logger.Log.Error(fmt.Sprintf("Not valid token %s", err.Error()))
+		return common.DoApiResponse(ctx, 500, nil, err)
+	}
+
+	if user == nil {
+		logger.Log.Warn("Not found user by token", "token", token)
+		return common.DoApiResponse(ctx, 401, nil, err)
+	}
+
+	ctx.Locals("user", user)
+
+	return ctx.Next()
+}
