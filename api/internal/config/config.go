b885645403885c1093346c8c5860b53fe077ae04
augustjourney <53155077+augustjourney@users.noreply.github.com>
2024-04-29 12:21:36 +0300

    feat: added config

--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Config struct {
+	BaseURL              string        `env:"BASE_URL"`
+	ServerAddress        string        `env:"SERVER_ADDRESS"`
+	DatabaseDSN          string        `env:"DATABASE_DSN"`
+	JWT_TOKEN_EXP        time.Duration `env:"JWT_TOKEN_EXP"`
+	JWT_TOKEN_SECRET_KEY string        `env:"JWT_TOKEN_SECRET_KEY"`
+	JWT_SIGNING_METHOD   jwt.SigningMethod
+}
+
+var config Config
+
+func New() *Config {
+	var flagServerAddress = flag.String("a", "localhost:8080", "Server address on which server is running")
+	var flagDatabaseDSN = flag.String("d", "", "Database DSN")
+
+	config.JWT_SIGNING_METHOD = jwt.SigningMethodHS256
+	config.JWT_TOKEN_SECRET_KEY = "super-secret-key"
+	config.JWT_TOKEN_EXP = time.Second * 30
+	config.ServerAddress = *flagServerAddress
+
+	if databaseDSN := os.Getenv("DATABASE_DSN"); databaseDSN != "" {
+		config.DatabaseDSN = databaseDSN
+	}
+
+	if *flagDatabaseDSN != "" {
+		config.DatabaseDSN = *flagDatabaseDSN
+	}
+
+	return &config
+
+}
