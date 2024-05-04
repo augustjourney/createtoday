b249d21da655f7626a6d2424909e3409e5311bef
augustjourney <53155077+augustjourney@users.noreply.github.com>
2024-05-04 17:24:23 +0300

    refactor: hero app instance

---
+++ api/internal/hero/app.go
@@ -0,0 +1,29 @@
+package hero
+
+import (
+	"createtodayapi/internal/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/jmoiron/sqlx"
+)
+
+func NewHeroApp(db *sqlx.DB, config *config.Config, app *fiber.App) *fiber.App {
+
+	postgres := NewPostgresRepo(db)
+	memory := NewMemoryRepo()
+
+	emailsService := NewEmailService(config, memory)
+	service := NewService(postgres, config, emailsService)
+
+	controller := NewController(service)
+
+	hero := app.Group("/hero")
+
+	hero.Post("/auth/login", controller.Login)
+	hero.Post("/auth/login/get-magic-link", controller.GetMagicLink)
+	hero.Post("/auth/login/validate-magic-link", controller.ValidateMagicLink)
+	hero.Post("/auth/signup", controller.Signup)
+	hero.Get("/profile", AuthMiddleware(service), controller.GetProfile)
+	hero.Get("/courses", AuthMiddleware(service), controller.GetUserAccessibleProducts)
+
+	return app
+}
