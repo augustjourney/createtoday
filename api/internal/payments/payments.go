0f1ec484c4889ac98a53a70a045affbea13e3f1b
augustjourney <53155077+augustjourney@users.noreply.github.com>
2024-05-19 23:53:24 +0300

    feat: added factory method for payment system

--- api/internal/payments/payments.go
+++ api/internal/payments/payments.go
@@ -1,26 +1,35 @@
 package payments
 
-import "context"
+import (
+	"context"
+)
 
 type GetPaymentLinkPayload struct {
-	Login           string `json:"login"`
-	Password        string `json:"password"`
-	Amount          uint64 `json:"amount"`
-	Email           string `json:"email"`
-	Phone           string `json:"phone"`
-	Description     string `json:"description"`
-	OrderId         string `json:"order_id"`
-	SendReceipt     bool   `json:"send_receipt"`
-	ReceiptSettings *struct {
-		Taxation string `json:"taxation"`
-	} `json:"receipt_settings"`
+	Login           string      `json:"login"`
+	Password        string      `json:"password"`
+	Amount          uint64      `json:"amount"`
+	Email           string      `json:"email"`
+	Phone           string      `json:"phone"`
+	Description     string      `json:"description"`
+	OrderId         int64       `json:"order_id"`
+	SendReceipt     bool        `json:"send_receipt"`
+	ReceiptSettings interface{} `json:"receipt_settings"`
 }
 
 type GetPaymentLinkResult struct {
 	PaymentURL string `json:"payment_url"`
 	PaymentID  string `json:"payment_id"`
+	OrderID    int64  `json:"order_id"`
 }
 
 type PaymentSystem interface {
 	GetPaymentLink(ctx context.Context, payload GetPaymentLinkPayload) (*GetPaymentLinkResult, error)
+}
+
+func NewPaymentSystem(paymentSystemType string) PaymentSystem {
+	switch paymentSystemType {
+	case "tinkoff":
+		return NewTinkoff()
+	}
+	return nil
 }
