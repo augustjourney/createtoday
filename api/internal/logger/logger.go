688c169ae4b60249de5e86e6277f950d6bcbfd5d
augustjourney <53155077+augustjourney@users.noreply.github.com>
2024-05-20 19:26:58 +0300

    feat: improve logger

--- api/internal/logger/logger.go
+++ api/internal/logger/logger.go
@@ -1,16 +1,41 @@
 package logger
 
 import (
+	"context"
 	"log/slog"
 	"os"
 )
 
+type ContextValues struct {
+	RequestID string
+}
+
 var Log *slog.Logger
+var contextValues ContextValues
 
 func init() {
 	Log = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	contextValues = ContextValues{
+		RequestID: "request-id",
+	}
 }
 
 func New() *slog.Logger {
 	return Log
+}
+
+func putContextValuesToArgs(args []interface{}, ctx context.Context) []interface{} {
+	requestId := ctx.Value(contextValues.RequestID)
+	args = append(args, contextValues.RequestID, requestId)
+	return args
+}
+
+func Info(ctx context.Context, message string, args ...interface{}) {
+	args = putContextValuesToArgs(args, ctx)
+	Log.InfoContext(ctx, message, args...)
+}
+
+func Error(ctx context.Context, message string, args ...interface{}) {
+	args = putContextValuesToArgs(args, ctx)
+	Log.ErrorContext(ctx, message, args...)
 }
